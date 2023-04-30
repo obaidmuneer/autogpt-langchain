7d7e1f4d0c194424f664329b2dd1b2a437c5702f
obaidmuneer <obaidmuneer55@gmail.com>
2023-04-30 13:54:49 +0500

    prompt template

--- app.mjs
+++ app.mjs
@@ -4,7 +4,24 @@ import { PromptTemplate } from "langchain/prompts";
 import * as dotenv from 'dotenv'
 dotenv.config()
 
-const llm = new OpenAI({ temperature: 0 })
+const model = new OpenAI({ temperature: 0 })
 
-const res = await llm.call('Tell me good name for my crockery company')
-console.log(res);
+// const res = await model.call('Tell me good name for my crockery company')
+// console.log(res);
+
+// promp template 
+const title_template = new PromptTemplate({
+    template: "Tell me good name for my {title} company",
+    inputVariables: ['title']
+})
+const title_chain = new LLMChain({
+    llm: model,
+    prompt: title_template,
+    verbose: true
+})
+
+// const resA = await title_chain.run('Electronic') // will return text "TechEase Solutions"
+// console.log(resA);
+
+// const resB = await title_chain.call({ title: 'Socks' }) // will return obj { text: '\n\nSocktastic!' }
+// console.log(resB);
