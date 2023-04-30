c7d2656f26d83eed2515370e9a2f4d50e3b71a2f
obaidmuneer <obaidmuneer55@gmail.com>
2023-04-30 14:19:35 +0500

    SimpleSequentialChain

--- app.mjs
+++ app.mjs
@@ -1,5 +1,5 @@
 import { OpenAI } from "langchain/llms/openai";
-import { LLMChain } from "langchain/chains";
+import { LLMChain, SimpleSequentialChain, SequentialChain } from "langchain/chains";
 import { PromptTemplate } from "langchain/prompts";
 import * as dotenv from 'dotenv'
 dotenv.config()
@@ -14,6 +14,8 @@ const title_template = new PromptTemplate({
     template: "Tell me good name for my {title} company",
     inputVariables: ['title']
 })
+
+//Chain
 const title_chain = new LLMChain({
     llm: model,
     prompt: title_template,
@@ -25,3 +27,24 @@ const title_chain = new LLMChain({
 
 // const resB = await title_chain.call({ title: 'Socks' }) // will return obj { text: '\n\nSocktastic!' }
 // console.log(resB);
+
+//Chain multiple task together
+
+const desc_template = new PromptTemplate({
+    inputVariables: ['company_name'],
+    template: 'Tell me short description for the given company name : {company_name}'
+})
+
+const desc_chain = new LLMChain({
+    llm: model,
+    prompt: desc_template
+})
+
+//SimpleSequentialChain is for multiple single-input/single output 
+const simple_chain = new SimpleSequentialChain({ 
+    chains: [title_chain, desc_chain],
+    verbose: true
+})
+
+const res = await simple_chain.run('Crockery')
+console.log(res);
